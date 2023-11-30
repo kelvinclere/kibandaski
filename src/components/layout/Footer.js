5fb1ad165d9c6614fb2b67b2e1d750129dd5ac4e
dennocapi <dennocapi@gmail.com>
2023-11-30 17:55:49 +0300

    Privacy policy

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,21 +1,31 @@
+import { Link } from "react-router-dom";
 import classes from "./Footer.module.css";
 import mealsImage from "../../assets/google.jpeg";
 import logoImage from "../../assets/Mautamu 4 Alt.png";
-import facebookImage from "../../assets/facebook.png";  // import the Facebook icon image
-import twitterImage from "../../assets/twitter.png"
-import emailImage from "../../assets/gmail.png"
-
+import facebookImage from "../../assets/facebook.png"; // import the Facebook icon image
+import twitterImage from "../../assets/twitter.png";
+import emailImage from "../../assets/gmail.png";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 const Footer = (props) => {
   return (
     <>
       <footer className={classes.footer}>
         <div className="social">
-          <img className={classes.logo} id="logo" src={logoImage} alt="logo image!" />
+          <img
+            className={classes.logo}
+            id="logo"
+            src={logoImage}
+            alt="logo image!"
+          />
           <div>
-          <img className={classes.playstore} id="playstore" src={mealsImage} alt="Google Play icon!" />
+            <img
+              className={classes.playstore}
+              id="playstore"
+              src={mealsImage}
+              alt="Google Play icon!"
+            />
           </div>
-         
         </div>
         <div>
           <h1>Contact Us</h1>
@@ -23,21 +33,26 @@ const Footer = (props) => {
           <h3>Email:Kibadaski@gmail.com</h3>
           <h3>Phone:0790793622</h3>
           <div id="media">
-          <a href="https://www.facebook.com/"><img src={facebookImage} alt="Facebook icon!" /></a> 
-          <a href="https://www.email.com/"><img src={emailImage} alt="Email icon!" /></a> 
-          <a href="https://www.twitter.com/"><img src={twitterImage} alt="Twitter icon!" /></a> 
+            <a href="https://www.facebook.com/">
+              <img src={facebookImage} alt="Facebook icon!" />
+            </a>
+            <a href="https://www.email.com/">
+              <img src={emailImage} alt="Email icon!" />
+            </a>
+            <a href="https://www.twitter.com/">
+              <img src={twitterImage} alt="Twitter icon!" />
+            </a>
           </div>
-           
         </div>
         <div>
-        <h1>Information</h1>
-        <div>
-        <a href="#">Refund Policy</a>
-        </div>
-        <div>
-        <a href="#">Privacy Policy</a>
-        </div>
-        <a href="#">Terms & Conditions</a>
+          <h1>Information</h1>
+          <div>
+            <a href="#">Refund Policy</a>
+          </div>
+          <div>
+            <Link to="/privacy-policy">Privacy Policy</Link> {PrivacyPolicy}
+          </div>
+          <a href="#">Terms & Conditions</a>
         </div>
       </footer>
     </>
@@ -45,4 +60,3 @@ const Footer = (props) => {
 };
 
 export default Footer;
-
